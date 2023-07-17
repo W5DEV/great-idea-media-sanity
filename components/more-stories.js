281d25d56d4aed2ecc0f5bf74dd44e69c8555c0c
John Larson <jl@buildmidwestern.com>
2023-07-17 13:15:29 -0500

    chore: updated size of more stories

--- components/more-stories.js
+++ components/more-stories.js
@@ -3,10 +3,10 @@ import PostPreview from '../components/post-preview'
 export default function MoreStories({ posts }) {
   return (
     <section>
-      <h2 className="mb-8 text-6xl md:text-7xl font-bold tracking-tighter leading-tight">
+      <h2 className="mb-8 text-4xl font-bold leading-tight tracking-tighter md:text-5xl">
         More Stories
       </h2>
-      <div className="grid grid-cols-1 md:grid-cols-2 md:gap-x-16 lg:gap-x-32 gap-y-20 md:gap-y-32 mb-32">
+      <div className="grid grid-cols-1 mb-32 md:grid-cols-2 md:gap-x-16 lg:gap-x-32 gap-y-20 md:gap-y-32">
         {posts.map((post) => (
           <PostPreview
             key={post.slug}
@@ -20,5 +20,5 @@ export default function MoreStories({ posts }) {
         ))}
       </div>
     </section>
-  )
+  );
 }
