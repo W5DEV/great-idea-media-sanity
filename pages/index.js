13543d82b4d1af604ba99a046efeec22292e24c2
John Larson <jl@buildmidwestern.com>
2023-07-17 13:22:52 -0500

    updated head

--- pages/index.js
+++ pages/index.js
@@ -14,7 +14,7 @@ export default function Index({ preview, allPosts }) {
     <>
       <Layout preview={preview}>
         <Head>
-          <title>{`Next.js Blog Example with ${CMS_NAME}`}</title>
+          <title>News and Sports. Nothing added.</title>
         </Head>
         <Container>
           {heroPost && (
