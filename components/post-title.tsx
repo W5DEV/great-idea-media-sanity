96954bd3c48860b10e68f471a4c8e92a76c227eb
John Larson <jl@buildmidwestern.com>
2023-07-18 05:38:45 -0500

    chore: removed files

--- components/post-title.tsx
+++
@@ -1,7 +0,0 @@
-export default function PostTitle({ children }) {
-  return (
-    <h1 className="mb-12 text-4xl font-bold leading-tight tracking-tighter text-center md:text-5xl lg:text-6xl md:leading-none md:text-left">
-      {children}
-    </h1>
-  );
-}
