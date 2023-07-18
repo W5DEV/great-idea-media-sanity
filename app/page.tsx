4310d748e22326a7d072bf44ff3a77fe3bc5fc5c
John Larson <jl@buildmidwestern.com>
2023-07-18 09:21:51 -0500

    overhauled homepage

--- app/page.tsx
+++ app/page.tsx
@@ -25,35 +25,7 @@ async function Home() {
   return (
     <>
       <NavBar news={news} sports={sports} newest={blogPosts[0]} />
-      <main className="p-[6vw]">
-        <div className="prose">
-          <ul>
-            {blogPosts.map((blogPost) => {
-              return (
-                <li
-                  key={blogPost.slug}
-                  className="w-2/5 p-4 my-2 border border-black"
-                >
-                  <Link href={`/${blogPost.slug}`}>
-                    <Image
-                      alt={blogPost.coverImage.fields.file.fileName}
-                      src={"https:" + blogPost.coverImage.fields.file.url}
-                      width={240}
-                      height={240}
-                    />
-                    <div className="flex flex-col items-start justify-start">
-                      <span>{blogPost.title}</span>
-                      <span className="italic">
-                        by: {blogPost.author.fields.name}
-                      </span>
-                    </div>
-                  </Link>
-                </li>
-              );
-            })}
-          </ul>
-        </div>
-      </main>
+      
     </>
   );
 }
